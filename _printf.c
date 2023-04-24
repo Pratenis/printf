3fe6b7bf8344496a33a93aca09a0e1ee2280bb74
Pratenis <ogonjiflevian@gmail.com>
2023-04-24 03:57:24 -0700

    update

--- _printf.c
+++ _printf.c
@@ -3,9 +3,9 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - Its function
+ * @format: Its format
+ * Return: Return chars
  */
 int _printf(const char *format, ...)
 {
@@ -26,8 +26,7 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
+				printed_chars++;
 		}
 		else
 		{
@@ -53,9 +52,10 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
+ * print_buffer - Prints buffer content
  * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * @buff_ind: Index
+ * Return: (Nothing)
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
